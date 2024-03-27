53f034ec5f840a5044e396bdac8ec5156d1748f1
Austin Frick <austin.frick429@gmail.com>
2024-03-27 18:21:58 -0400

    added code to display only the name clicked

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchPokedexEntries() {
     const pokedexUrl = 'https://pokeapi.co/api/v2/pokedex/1/';
-    
+    let currentPokemon = null ;
     fetch(pokedexUrl)
         .then(function(response) {
             if (!response.ok) {
@@ -11,9 +11,9 @@ function fetchPokedexEntries() {
         .then(function(data) {
             const entries = data.pokemon_entries.slice(242, 251); // setting up the specific entries you need 
 
-            const entryList = document.getElementById('pokemon-entries');
+            const entryList = document.querySelector('#pokemon-entries');
 
-        
+            console.log(entries)
             entryList.innerHTML = '';
 
             // Loops through the fetched entries and create list items
@@ -24,11 +24,14 @@ function fetchPokedexEntries() {
                 
                 listItem.addEventListener('click', ()=>{
                     const pokemonName = entry.pokemon_species.name; 
-                    
+                    if (currentPokemon) {
+                        currentPokemon.remove ();
+                    }
                     let img = document.createElement('img');
                     img.src = `assets/${pokemonName}.jpg`;
                     img.alt = pokemonName; 
                     entryList.appendChild(img); 
+                    currentPokemon = img
                 });
             });
         })
